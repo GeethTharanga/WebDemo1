dfbad6322247d5733ef8e8fde17b51fff77813ec
GeethTharanga <getthh@gmail.com>
2014-03-06 10:22:57 +0530

    added error handling

--- WebDemo1/Util/WebExAdapter.cs
+++ WebDemo1/Util/WebExAdapter.cs
@@ -8,6 +8,25 @@ using System.Globalization;
 
 namespace WebDemo1.Util
 {
+    [Serializable]
+    public class WebExException : Exception
+    {
+        public WebExErrorDetails WebExError { get; set; }
+
+        public WebExException(WebExErrorDetails err) : base(err.Reason) { WebExError = err; }
+        public WebExException(WebExErrorDetails err, Exception inner) : base(err.Reason, inner) { WebExError = err; }
+        protected WebExException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+
+    public class WebExErrorDetails
+    {
+        public int ExceptionID { get; set; }
+        public string Reason { get; set; }
+    }
+
     public class WebExAdapter
     {
         const string strBodyContent = "bodyContent";
@@ -16,6 +35,28 @@ namespace WebDemo1.Util
         const string strReqMeelingList = "java:com.webex.service.binding.meeting.LstsummaryMeeting";
 
         const string strNamespaceServ = "http://www.webex.com/schemas/2002/06/service", strNamespaceMeet = "http://www.webex.com/schemas/2002/06/service/meeting";
+
+
+        private WebExErrorDetails GetErrorDetails(XDocument xdoc)
+        {
+            XNamespace nsServ = strNamespaceServ;
+
+            XElement respNode = xdoc.Element(nsServ + "response");
+
+            if (respNode.Element(nsServ + "result").Value != "FAILURE")
+            {
+                return null;
+            }
+            else
+            {
+                var error = new WebExErrorDetails {  ExceptionID = int.Parse(respNode.Element(nsServ + "exceptionID").Value),
+                                              Reason = respNode.Element(nsServ + "reason").Value };
+
+                return error;
+            }
+
+        }
+
 
         public IEnumerable<MeetingSummary> GetMeetingList()
         {
@@ -30,6 +71,13 @@ namespace WebDemo1.Util
 
             XDocument doc = XDocument.Parse(result);
 
+            WebExErrorDetails err = GetErrorDetails(doc);
+
+            if (err != null)
+            {
+                throw new WebExException(err);
+            }
+
             XNamespace nsMeet = strNamespaceMeet;
             var nodes = doc.Descendants(nsMeet + "meeting").Select(elem => new MeetingSummary
                                     {
@@ -39,8 +87,7 @@ namespace WebDemo1.Util
                                         StartDate = DateTime.ParseExact(elem.Element(nsMeet + "startDate").Value, dateFormat, CultureInfo.InvariantCulture),
                                         Duration = int.Parse(elem.Element(nsMeet + "duration").Value)
                                     }).ToList();
-
-            return nodes;
+                        return nodes;
         }
     }
 }
